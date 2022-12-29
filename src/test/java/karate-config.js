424e361fea79e00101a167919a5c607eb57632e1
Alberto <alberto.zapata.orta@gmail.com>
2022-12-28 20:40:47 -0500

    add name & job global variable

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,10 +1,14 @@
 function fn() {
+  var env = karate.env;
+  karate.log('karate.env system property was:', env);
   if (!env) {
     env = 'dev';
   }
   var config = {
+    env: env,
     baseUrl: "https://reqres.in",
-    myVarName: 'someValue'
+    name: 'Karate Framework',
+    job: 'API Test Automation'
   }
   if (env == 'dev') {
     // customize
